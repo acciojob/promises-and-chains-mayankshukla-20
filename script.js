8a5669ecdf2539c19fce3266c398ed0d3b436229
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2024-01-25 16:18:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const name = document.getElementById("name").value;
+const age = document.getElementById("age").value;
+
+document.getElementById("btn").addEventListener("click" , display());
+
+function display(){
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			if(age>=18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		}, 4000)
+	})
+}
+
+display().then((data) => {
+	alert(data);
+}).catch((err) => alert(err));
+	
